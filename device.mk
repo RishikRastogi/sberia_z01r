adb3eeda1a7cb533af61028f503092da9b4b4249
RishikRastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-09-25 16:25:10 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     libdng_sdk.vendor \
     Snap \
-#vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0
 
 # Common init scripts
 PRODUCT_PACKAGES += \
